fc111dcd28cdc332c15c122a13be56c0616d878d
alejandrobrnalchavez <alejogevara27@gmail.com>
2025-04-01 09:46:30 -0600

    PARCIAL

--- componentes/autor.js
+++ componentes/autor.js
@@ -7,7 +7,7 @@ const autor = {
             codigo: '',
             nombre: '',
             pais: '',
-            telefono: ''
+            telefono: '',
         }
     },
     methods: {
@@ -31,9 +31,9 @@ const autor = {
                 telefono: this.telefono
             };
             if (this.accion == 'modificar') {
-                alumno.idAutor = this.idAutor;
+                autor.idAutor = this.idAutor;
             }
-            db.autor.put(autor);
+            db.autores.put(autor);
             this.nuevoAutor();
         },
         nuevoAutor() {
@@ -46,45 +46,40 @@ const autor = {
         }
     },
     template: `
-        <div class="row">
-            <div class="col-6">
-                <form id="frmAutor" name="frmAutor" @submit.prevent="guardarAutor">
-                    <div class="card border-dark mb-3">
-                        <div class="card-header bg-dark text-white">Registro de Autor</div>
-                        <div class="card-body">
-                            <div class="row p-1">
-                                <div class="col-3 col-md-2">CODIGO</div>
-                                <div class="col-9 col-md-4">
-                                    <input required v-model="codigo" type="text" name="txtCodigoAutor" id="txtCodigoAutor" class="form-control">
+        <div class="container mt-4">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form id="frmAutor" name="frmAutor" @submit.prevent="guardarAutor">
+                        <div class="card shadow-sm border-0">
+                            <div class="card-header bg-primary text-white text-center fw-bold">
+                                Registro de Autores
+                            </div>
+                            <div class="card-body">
+                                <div class="mb-3">
+                                    <label class="form-label fw-semibold">Código</label>
+                                    <input required v-model="codigo" type="text" class="form-control">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fw-semibold">Nombre</label>
+                                    <input required pattern="[A-Za-zñÑáéíóú ]{3,150}" v-model="nombre" type="text" class="form-control">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fw-semibold">País</label>
+                                    <input required v-model="pais" type="text" class="form-control">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fw-semibold">Teléfono</label>
+                                    <input v-model="telefono" type="text" class="form-control">
                                 </div>
                             </div>
-                            <div class="row p-1">
-                                <div class="col-3 col-md-2">NOMBRE</div>
-                                <div class="col-9 col-md-6">
-                                    <input required pattern="[A-Za-zñÑáéíóú ]{3,150}" v-model="nombre" type="text" name="txtNombreAutor" id="txtNombreAutor" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row p-1">
-                                <div class="col-3 col-md-2">PAIS</div>
-                                <div class="col-9 col-md-8">
-                                    <input required v-model="pais" type="text" name="txtPaisAutor" id="txtPaisAutor" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row p-1">
-                                <div class="col-3 col-md-2">TELEFONO</div>
-                                <div class="col-9 col-md-4">
-                                    <input v-model="telefono" type="text" name="txtTelefonoAutor" id="txtTelefonoAutor" class="form-control">
-                                </div>
-                            </div>
+                            <div class="card-footer text-center">
+                                <button type="submit" class="btn btn-success me-2">Guardar</button>
+                                <button type="reset" @click="nuevoAutor" class="btn btn-warning me-2">Nuevo</button>
+                                <button type="button" @click="buscarAutor" class="btn btn-info">Buscar</button>
                             </div>
                         </div>
-                        <div class="card-footer bg-dark text-center">
-                            <input type="submit" value="Guardar" class="btn btn-primary"> 
-                            <input type="reset" value="Nuevo" class="btn btn-warning">
-                            <input type="button" @click="buscarAutor" value="Buscar" class="btn btn-info">
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     `
